c6d5ac15ddd4056aecf154b1e4941d444a3be3b3
devdas-snow <parag.devdas@gmail.com>
2024-10-03 15:55:12 +0530

    Update streamlit_app.py
    Added import request from FruityVice app

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,12 @@ if ingredients_list:
     time_to_insert = st.button('submit order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!'+name_on_order, icon="✅")    
+        st.success('Your Smoothie is ordered!'+name_on_order, icon="✅")
+
+# New section to display FruityVice Nutrition info
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
    
 
